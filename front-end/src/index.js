70ea90aaf89223ff13459988c745212954136c56
Devin Brueberg <brueberd@csp.edu>
2022-12-22 11:24:50 -0600

    React Router Dom is added to build

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,22 @@
+// Initially Created by: Devin Brueberg
+// 2022 Side Project
+// Gold-Briefing - index.js
+// December 22, 2022
+// Last Edited (Initials, Date, Edits):
+//  (DAB, 12/22/2022, Added in Browser Router for SPA page navigation)
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
