20e4456b698d9165b8ff2df668d0fe0652f9d730
Devin Brueberg <brueberd@gmail.com>
2023-01-04 20:28:20 -0600

    Weather Card Layout is complete. Now to finish the Map Layout

--- front-end/src/components/subComponent/Widget.js
+++ front-end/src/components/subComponent/Widget.js
@@ -7,40 +7,162 @@
 // Using React library in order to build components
 // for the app and importing needed components
 import React from "react";
-import { Box, Button, Card, Typography } from "@mui/material";
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Box,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Grid,
+    List,
+    ListItem,
+    ListItemButton,
+    Paper,
+    Tooltip,
+    Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import WeatherDataService from "../../services/weather.service";
+import sampleData from "../../redux/sampleData.json";
 
 /**
- * The Widget Component will display widgets of the weather and 
+ * The Widget Component will display widgets of the weather and
  * map based off GPS location
  *
  * @returns
  */
 function Widget(props) {
     // Destructuring the needed methods from props
-    const {  } = props;
+    const { onClickUpdateWeather } = props;
     // Destructuring the needed variable from props
-    const {  } = props;
+    const { weather } = props;
+    // const { weather } = sampleData
 
-    const clickHandler = async() => {
-        console.log("Button clicked")
-        const response = await WeatherDataService.forecast("minneapolis, mn 55304");
-        console.log(response);
-        console.log(response.data);
-    }
+    const clickHandler = () => {
+        onClickUpdateWeather();
+    };
+
+    const WeatherAlerts = () => (
+        <Accordion>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="weatherAlert"
+            >
+                <Typography fontWeight={"400"} color="error">
+                    Weather Alerts!
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <List sx={{ p: 0, m: 0 }}>
+                    {weather &&
+                        weather?.alerts?.length > 0 &&
+                        weather.alerts.map((alert, index) => (
+                            <Tooltip
+                                key={index}
+                                placement="top"
+                                title={alert.description}
+                            >
+                                <ListItemButton>{alert.event}</ListItemButton>
+                            </Tooltip>
+                        ))}
+                </List>
+            </AccordionDetails>
+        </Accordion>
+    );
+
+    const WeatherCard = () => (
+        <Card display="flex" sx={{ height: "100%" }}>
+            <CardContent>
+                <Typography variant="h6">{weather.weatherLocation} Weather</Typography>
+                <Typography variant="h7" fontSize={".9rem"}>
+                    {weather.currentCondition}
+                </Typography>
+                <Grid container sx={{ marginBottom: "2rem" }}>
+                    <Grid item xs={12} sm={6} lg={3}>
+                        <Typography fontSize={"small"}>
+                            Rain: {weather.rain}%
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={3}>
+                        <Typography fontSize={"small"}>
+                            Snow: {weather.snow}%
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={3}>
+                        <Typography fontSize={"small"}>
+                            Temp: {weather.temp}degF
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={3}>
+                        <Typography fontSize={"small"}>
+                            R-Feel: {weather.realFeel}degF
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={3}>
+                        <Typography fontSize={"small"}>
+                            Wind: {weather.wind}mph
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={3}>
+                        <Typography fontSize={"small"}>
+                            Gusts: {weather.gust}mph
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={3}>
+                        <Typography fontSize={"small"}>
+                            Wind Dir: {weather.windDirection}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={3}>
+                        <Typography fontSize={"small"}>
+                            UV: {weather.uV}
+                        </Typography>
+                    </Grid>
+                </Grid>
+                {weather && weather?.alerts?.length > 0 && <WeatherAlerts />}
+                <CardActions
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                    }}
+                >
+                    <Button size="small" onClick={() => clickHandler()}>
+                        Update Weather
+                    </Button>
+                </CardActions>
+            </CardContent>
+        </Card>
+    );
 
     return (
         <Box>
-            <Typography>
-                {/** I want  weather alerts, wind speed if over 30mph from current time */}
-                Widget Component
-            </Typography>
-            <Card>
-
-            </Card>
-            <Button onClick={() => clickHandler()}>
-                Update Weather
-            </Button>
+            <Grid container spacing={1} sx={{ maxWidth: "100%" }}>
+                {weather && (
+                    <Grid item xs={12} sm={6}>
+                        <Paper
+                            elevation={2}
+                            sx={{ height: "100%", marginLeft: 1 }}
+                        >
+                            <WeatherCard />
+                        </Paper>
+                    </Grid>
+                )}
+                <Grid item xs={12} sm={6}>
+                    <Paper elevation={2} sx={{ height: "100%", marginLeft: 1 }}>
+                        <Card sx={{ height: "100%" }}>
+                            <CardContent>
+                                <Typography gutterBottom variant="h6">
+                                    Map
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Paper>
+                </Grid>
+            </Grid>
         </Box>
     );
 }
