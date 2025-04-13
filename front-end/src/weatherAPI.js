74d9cf0a3c922d8fffd15da7786db3fdcc148533
DBrueberg <brueberg@gmail.com>
2025-04-12 21:15:10 -0500

    Completed initial remote launch of gold briefing

--- front-end/src/weatherAPI.js
+++ front-end/src/weatherAPI.js
@@ -8,7 +8,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://api.weatherapi.com/v1/",
+  baseURL: "https://api.weatherapi.com/v1/",
   headers: {
     "Content-type": "application/json"
   }
