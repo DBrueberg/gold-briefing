da6ef37f96d935f5fd3707a7728efd27ff9bd994
Devin Brueberg <brueberg@gmail.com>
2025-04-19 23:21:14 -0500

    Removed some debug code that was caught in merge req

--- back-end/controllers/user.controller.js
+++ back-end/controllers/user.controller.js
@@ -26,9 +26,6 @@ exports.create = async (req, res) => {
         return;
     }
 
-    console.log(req.body);
-    console.log(req.body.email);
-
     // Initializing user data
     let user = {
         fName: req.body.fName,
