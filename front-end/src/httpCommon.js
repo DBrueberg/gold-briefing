fe718958f05deb280eab9ac12a1c9a963c7d7102
Devin Brueberg <brueberg@gmail.com>
2025-04-26 19:32:11 -0500

    checkEnv still not working, going back to the single line on httpCommon.js

--- front-end/src/httpCommon.js
+++ front-end/src/httpCommon.js
@@ -15,7 +15,8 @@ const isProd = checkEnv();
 // to figure out the correct way to write this
 // Creating the axios instance with the base URL and headers
 export default axios.create({
-    baseURL: isProd ? "gold-briefingback-end-production.up.railway.app" : "http://localhost:5000",
+    // baseURL: isProd ? "gold-briefingback-end-production.up.railway.app" : "http://localhost:5000",
+    baseURL: "gold-briefingback-end-production.up.railway.app",
     headers: {
         "Content-type": "application/json",
     },
